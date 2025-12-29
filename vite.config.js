2671960d42f2ecddbeda7615faacc1e681b8d9c2
enzoDemo <enzo@demo.expertizen.com>
2025-12-29 14:31:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Actualizamos el target a es2020 para soportar import.meta.env
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  }
 })
