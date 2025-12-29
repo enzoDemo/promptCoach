e4331e4e82b6c7a6160878cb4c230409a83ec0ae
enzoDemo <enzo@demo.expertizen.com>
2025-12-29 11:21:25 -0300

    Update main.jsx
    v2

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // Opcional si usas css puro, pero necesario si Vite lo pide
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
